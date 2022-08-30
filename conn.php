3d050a5c230829ef1a71013a8ab17c5fe8f83303
hotdy <111363321+assd123ss@users.noreply.github.com>
2022-08-30 16:44:08 +0700

    c

--- conn.php
+++
@@ -1,3 +0,0 @@
-<?php
-   $conn = new mysqli('dberp.czr48qkizqnc.ap-southeast-1.rds.amazonaws.com','admin','0881085759Hot','dberp');
-?>
---
+++ connection.php
@@ -0,0 +1,23 @@
+<?php
+
+$conn = "";
+
+try {
+	$servername = "dberp.czr48qkizqnc.ap-southeast-1.rds.amazonaws.com";
+	$dbname = "dberp";
+	$username = "admin";
+	$password = "0881085759Hot";
+
+	$conn = new PDO(
+		"mysql:host=$servername; dbname=dberp.czr48qkizqnc.ap-southeast-1.rds.amazonaws.com",
+		$username, $password
+	);
+	
+$conn->setAttribute(PDO::ATTR_ERRMODE,
+					PDO::ERRMODE_EXCEPTION);
+}
+catch(PDOException $e) {
+	echo "Connection failed: " . $e->getMessage();
+}
+
+?>
