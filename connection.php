dd8f2377827bcebca40174b707fad19f3edc6fc0
hotdy <111363321+assd123ss@users.noreply.github.com>
2022-08-30 17:41:12 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -3,13 +3,13 @@
 $conn = new mysqli($servername, $username, $password);
 
 try {
-	$servername = "dberp.czr48qkizqnc.ap-southeast-1.rds.amazonaws";
+	$servername = "dberp.czr48qkizqnc.ap-southeast-1.rds.amazonaws.com";
 	$dbname = "dberp";
 	$username = "admin";
 	$password = "0881085759Hot";
 
 	$conn = new PDO(
-		"mysql:host=$servername; dbname=dberp.czr48qkizqnc.ap-southeast-1.rds.amazonaws",
+		"mysql:host=$servername; dbname=dberp.czr48qkizqnc.ap-southeast-1.rds.amazonaws.com",
 		$username, $password
 	);
 	
