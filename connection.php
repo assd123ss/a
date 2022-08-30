d39b4068d3c948ff326db8638efab27cf4a02823
hotdy <111363321+assd123ss@users.noreply.github.com>
2022-08-30 18:58:47 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -9,7 +9,7 @@ try {
 	$password = "0881085759Hot";
 
 	$conn = new PDO(
-		"mysql:host=$servername; dbname=dberp.czr48qkizqnc.ap-southeast-1.rds.amazonaws.com",
+		"mysql:host=$servername; dbname=dberp",
 		$username, $password
 	);
 	
