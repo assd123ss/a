d09234cdbb1348681c2434b9a8ea4d2547e6adf1
assd123ss <111363321+assd123ss@users.noreply.github.com>
2022-08-31 20:45:22 +0700

    Update stu.php

--- stu.php
+++ stu.php
@@ -55,10 +55,9 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <div class="card" style="width: 18rem;">
-  <img src="https://cdn.pixabay.com/photo/2021/11/12/12/16/leaves-6788800_960_720.jpg" class="card-img-top">
+   
 
-  
+ <div class="container">
   <div class="card-group">
   <div class="card">
     <img src="https://cdn.pixabay.com/photo/2021/11/12/12/16/leaves-6788800_960_720.jpg" class="card-img-top" >
@@ -82,6 +81,7 @@
       <h5 class="card-title">Certificate Manager</h5>
       <p class="card-text">สร้าง www.ชื่อทีเราสร้างจากเว็บz กับ ไม่ www. ชื่อที่เราสร้างจากเว็บZ</p>
       <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+</div>
     </div>
   </div>
 </div>
