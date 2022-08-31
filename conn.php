8c59f17cf5676f7445140b32ddca2732d289abb8
assd123ss <111363321+assd123ss@users.noreply.github.com>
2022-08-31 17:35:32 +0700

    aaa

--- conn.php
+++ conn.php
@@ -1,6 +1,3 @@
 <?php
-
-
 $conn = mysqli_connect("dberp.czr48qkizqnc.ap-southeast-1.rds.amazonaws.com","admin","0881085759Hot","dberp");
-
 ?>
--- index.php
+++ index.php
@@ -1,37 +1,31 @@
+<?php
+include "conn.php";
+?>
+
 <!DOCTYPE html>
-<html lang="en">
-
+<html lang="en" >
 <head>
-	<meta charset="UTF-8">
-	<link rel="stylesheet" href=
-"https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<link rel="stylesheet" href="login.css">
-	<title>Login Page</title>
+  <meta charset="UTF-8">
+  <title>Login Page with Background Image Example</title>
+  <link rel="stylesheet" href="./style.css">
+
 </head>
-
 <body>
-	<form action="validate.php" method="post">
-		<div class="login-box">
-			<h1>Login</h1>
+<div id="bg"></div>
 
-			<div class="textbox">
-				<i class="fa fa-user" aria-hidden="true"></i>
-				<input type="text" placeholder="Username"
-						name="username" value="">
-			</div>
-
-			<div class="textbox">
-				<i class="fa fa-lock" aria-hidden="true"></i>
-				<input type="password" placeholder="Password"
-						name="password" value="">
-			</div>
-
-			<input class="button" type="submit"
-					name="login" value="Sign In">
-		</div>
-	</form>
+<form>
+  <div class="form-field">
+    <input type="text" placeholder="Email / Username" required/>
+  </div>
+  
+  <div class="form-field">
+    <input type="password" placeholder="Password" required/>                         </div>
+  
+  <div class="form-field">
+    <button class="btn" type="submit">Log in</button>
+  </div>
+</form>
+<!-- partial -->
+  
 </body>
-
 </html>
