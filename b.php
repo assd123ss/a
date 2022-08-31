62c71ecaa597812271316420b505b012cc07e542
assd123ss <111363321+assd123ss@users.noreply.github.com>
2022-08-31 21:52:29 +0700

    Update b.php

--- b.php
+++ b.php
@@ -51,6 +51,7 @@ while ($row = mysqli_fetch_assoc($result)) {
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <div class="bg-image"  style ="background-image: url('https://cdn.pixabay.com/photo/2022/08/21/12/05/lightning-7401119_960_720.jpg'); height: 100vh;"> </div>
   </body>
 </html>
 
