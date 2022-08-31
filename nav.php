013de779d176daa4470c932feb206ab0801b5392
assd123ss <111363321+assd123ss@users.noreply.github.com>
2022-08-31 19:55:05 +0700

    Update nav.php

--- nav.php
+++ nav.php
@@ -8,6 +8,7 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
   </head>
   <body>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -24,6 +25,7 @@
         <li class="nav-item">
           <a class="nav-link" href="#">Logout</a>
         </li>
+
 
     <div class="dropdown">
       <a class="dropdown-toggle" data-toggle="dropdown" href="#">Personal asset loans</a>
