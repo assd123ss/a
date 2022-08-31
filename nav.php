e1da1a03d43af95ddfe786ae39bc022dfa044d42
assd123ss <111363321+assd123ss@users.noreply.github.com>
2022-08-31 21:28:25 +0700

    aaa

--- nav.php
+++ nav.php
@@ -9,7 +9,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="./a.css">
+   
   </head>
   <body>
   <nav class="navbar navbar-expand-md navbar-dark bg-dark">
@@ -33,7 +33,7 @@
     </div>
     <div class="mx-auto order-0">
       <a class="navbar-brand mx-auto" href="nav.php">HOT SEN WEB</a>
-      <div class="bg"></div>
+       <div class="bg"></div>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
         <span class="navbar-toggler-icon"></span>
       </button>
