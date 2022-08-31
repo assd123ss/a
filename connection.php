50a19da63ae9e0a29d16a05fbc0083fef3348c7e
assd123ss <111363321+assd123ss@users.noreply.github.com>
2022-08-31 17:16:31 +0700

    aa

---
+++ connection.php
@@ -0,0 +1,23 @@
+<?php
+
+$conn = "";
+
+try {
+	$servername = "dberp.czr48qkizqnc.ap-southeast-1.rds.amazonaws.com";
+	$dbname = "dberp";
+	$username = "admin";
+	$password = "0881085759Hot";
+
+	$conn = new PDO(
+		"mysql:host=$servername; dbname=dberp",
+		$username, $password
+	);
+	
+$conn->setAttribute(PDO::ATTR_ERRMODE,
+					PDO::ERRMODE_EXCEPTION);
+}
+catch(PDOException $e) {
+	echo "Connection failed: " . $e->getMessage();
+}
+
+?>
