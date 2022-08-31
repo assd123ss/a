221e18be1c265ccd140cda1ba91a9faabe9c8fa6
assd123ss <111363321+assd123ss@users.noreply.github.com>
2022-08-31 18:13:08 +0700

    dd

--- ai.php
+++ ai.php
@@ -7,7 +7,7 @@ if(isset($_POST['username'])&&isset($_POST['password'])) {
    $sql="select * from adminlogin where username ='$username' and password = '$password'";
    $result = mysqli_query($conn,$sql);
    if (mysqli_num_rows($result) == 1) {
-  echo"<script>window.location='b.php'</script>";
+  echo"<script>window.location='Register.php'</script>";
   
 
 
