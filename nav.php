55567539c536523788102a6bd83c60162dfbad74
assd123ss <111363321+assd123ss@users.noreply.github.com>
2022-09-01 19:41:01 +0700

    update

--- nav.php
+++ nav.php
@@ -1,58 +1,90 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <title>Title</title>
+
+<head>
+    <title>--------หน้าหลัก------</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
 
-   
-  </head>
-  <body>
-  <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-  <div class="container-fluid">
-    <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
-      <ul class="navbar-nav me-auto">
-        <li class="nav-item active">
-        <li class="nav-item">
-          <a class="nav-link" href="logout.php">Logout</a>
-        </li>
-        <div class="dropdown">
-  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-   ข้อมูลต่างๆ
-  </button>
-  <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-    <li><a class="dropdown-item" href="b.php">ข้อมูลแสดงผู้ใช้-รหัส</a></li>
-    <li><a class="dropdown-item" href="stu.php">ข้อมูลที่เรียน</a></li>
-  </ul>
-</div>
-      </ul>
-    </div>
-    <div class="mx-auto order-0">
-      <a class="navbar-brand mx-auto" href="nav.php">HOT SEN WEB</a>
-     
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-    </div>
-    
-    <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
-      <ul class="navbar-nav ms-auto">
-      </ul>
-    </div>
-  </div>
-</nav>
+
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+        <div class="container-fluid">
+            <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item active">
+                    <li class="nav-item">
+                        <a class="nav-link" href="logout.php">Logout</a>
+                    </li>
+                    <div class="dropdown">
+                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            ข้อมูลต่างๆ
+                        </button>
+                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                            <li><a class="dropdown-item" href="b.php">ข้อมูลแสดงผู้ใช้-รหัส</a></li>
+                            <li><a class="dropdown-item" href="stu.php">ข้อมูลที่เรียน</a></li>
+                        </ul>
+                    </div>
+                </ul>
+            </div>
+            <div class="mx-auto order-0">
+                <a class="navbar-brand mx-auto" href="nav.php">HOT SEN WEB</a>
+
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            </div>
+
+            <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+                <ul class="navbar-nav ms-auto">
+                </ul>
+            </div>
+        </div>
+    </nav>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
 
-  </div>
-</div>
-  </body>
+    </div>
+    </div>
+
+
+
+
+
+    <audio id="myAudio" controls autoplay>
+  <source src="horse.ogg" type="audio/ogg">
+  <source src="horse.mp3" type="audio/mpeg">
+  <p id="demo"></p>
+
+  
+</audio>
+<p id="demo"></p>
+<script>
+function myFunction() {
+  var x = document.getElementById("myAudio").autoplay;
+  document.getElementById("demo").innerHTML = x;
+}
+</script>
+</body>
+
 </html>
--- stu.php
+++ stu.php
@@ -1,142 +1,171 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <title>Title</title>
+
+<head>
+    <title>หน้าอัปเดต</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-  </head>
-  <body>
-  <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-  <div class="container-fluid">
-    <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
-      <ul class="navbar-nav me-auto">
-        <li class="nav-item">
-          <a class="nav-link" href="logout.php">Logout</a>
-        </li>
-        <div class="dropdown">
-  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-   ข้อมูลต่างๆ
-  </button>
-  <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-    <li><a class="dropdown-item" href="b.php">ข้อมูลแสดงผู้ใช้-รหัส</a></li>
-    <li><a class="dropdown-item" href="stu.php">ข้อมูลที่เรียน</a></li>
-  </ul>
-</div>
-      </ul>
-    </div>
-    <div class="mx-auto order-0">
-      <a class="navbar-brand mx-auto" href="nav.php">HOT SEN WEB</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-    </div>
-    <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
-      <ul class="navbar-nav ms-auto">
-      </ul>
-    </div>
-  </div>
-</nav>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+        <div class="container-fluid">
+            <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="logout.php">Logout</a>
+                    </li>
+                    <div class="dropdown">
+                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            ข้อมูลต่างๆ
+                        </button>
+                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                            <li><a class="dropdown-item" href="b.php">ข้อมูลแสดงผู้ใช้-รหัส</a></li>
+                            <li><a class="dropdown-item" href="stu.php">ข้อมูลที่เรียน</a></li>
+                        </ul>
+                    </div>
+                </ul>
+            </div>
+            <div class="mx-auto order-0">
+                <a class="navbar-brand mx-auto" href="nav.php">HOT SEN WEB</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            </div>
+            <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+                <ul class="navbar-nav ms-auto">
+                </ul>
+            </div>
+        </div>
+    </nav>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-   
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
 
- <div class="container">
-  <div class="card-group">
-  <div class="card">
-    <img src="https://cdn.pixabay.com/photo/2021/11/12/12/16/leaves-6788800_960_720.jpg" class="card-img-top" >
-    <div class="card-body">
-      <h5 class="card-title">เว็บz.com </h5>
-      <p class="card-text">เว็บz เอาค่าDNS จากroute 53 ลง เว็บ z</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-    </div>
-  </div>
-  <div class="card">
-    <img src="https://cdn.pixabay.com/photo/2021/11/12/12/16/leaves-6788800_960_720.jpg" class="card-img-top" >
-    <div class="card-body">
-      <h5 class="card-title">Route 53</h5>
-      <p class="card-text">สร้าง DNS เพื่อไปเชือมกับเว็บz</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-    </div>
-  </div>
-  <div class="card">
-    <img src="https://cdn.pixabay.com/photo/2021/11/12/12/16/leaves-6788800_960_720.jpg" class="card-img-top" >
-    <div class="card-body">
-      <h5 class="card-title">Certificate Manager</h5>
-      <p class="card-text">สร้าง www.ชื่อทีเราสร้างจากเว็บz กับ ไม่ www. ชื่อที่เราสร้างจากเว็บZ</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-</div>
-    </div>
 
     <div class="container">
-    <div class="card-group">
-  <div class="card">
-    <img src="https://cdn.pixabay.com/photo/2021/11/12/12/16/leaves-6788800_960_720.jpg" class="card-img-top" >
-    <div class="card-body">
-      <h5 class="card-title">Elastic Beanstalk </h5>
-      <p class="card-text">ตั้งค่า Load balancer และสร้าง https: post 443</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-    </div>
-  </div>
-  <div class="card">
-    <img src="https://cdn.pixabay.com/photo/2021/11/12/12/16/leaves-6788800_960_720.jpg" class="card-img-top" >
-    <div class="card-body">
-      <h5 class="card-title">Github</h5>
-      <p class="card-text">สร้าง และ โหลดมาไว้ในเครื่อง และ commitขึ้นgithub</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-    </div>
-  </div>
-  <div class="card">
-    <img src="https://cdn.pixabay.com/photo/2021/11/12/12/16/leaves-6788800_960_720.jpg" class="card-img-top" >
-    <div class="card-body">
-      <h5 class="card-title">Pipeline</h5>
-      <p class="card-text">สร้างและเชื่อมgithubเพื่อให้เป็นเว็บไซด์ได้แต่ไม่ใช่เว็บไซด์ทางการ</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-</div>
-</div>
+        <div class="card-group">
+            <div class="card">
+                <img src="https://cdn.pixabay.com/photo/2021/11/12/12/16/leaves-6788800_960_720.jpg"
+                    class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">เว็บz.com </h5>
+                    <p class="card-text">เว็บz เอาค่าDNS จากroute 53 ลง เว็บ z</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+            </div>
+            <div class="card">
+                <img src="https://cdn.pixabay.com/photo/2021/11/12/12/16/leaves-6788800_960_720.jpg"
+                    class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">Route 53</h5>
+                    <p class="card-text">สร้าง DNS เพื่อไปเชือมกับเว็บz</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+            </div>
+            <div class="card">
+                <img src="https://cdn.pixabay.com/photo/2021/11/12/12/16/leaves-6788800_960_720.jpg"
+                    class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">Certificate Manager</h5>
+                    <p class="card-text">สร้าง www.ชื่อทีเราสร้างจากเว็บz กับ ไม่ www. ชื่อที่เราสร้างจากเว็บZ</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+            </div>
+
+            <div class="container">
+                <div class="card-group">
+                    <div class="card">
+                        <img src="https://cdn.pixabay.com/photo/2021/11/12/12/16/leaves-6788800_960_720.jpg"
+                            class="card-img-top">
+                        <div class="card-body">
+                            <h5 class="card-title">Elastic Beanstalk </h5>
+                            <p class="card-text">ตั้งค่า Load balancer และสร้าง https: post 443</p>
+                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <img src="https://cdn.pixabay.com/photo/2021/11/12/12/16/leaves-6788800_960_720.jpg"
+                            class="card-img-top">
+                        <div class="card-body">
+                            <h5 class="card-title">Github</h5>
+                            <p class="card-text">สร้าง และ โหลดมาไว้ในเครื่อง และ commitขึ้นgithub</p>
+                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <img src="https://cdn.pixabay.com/photo/2021/11/12/12/16/leaves-6788800_960_720.jpg"
+                            class="card-img-top">
+                        <div class="card-body">
+                            <h5 class="card-title">Pipeline</h5>
+                            <p class="card-text">สร้างและเชื่อมgithubเพื่อให้เป็นเว็บไซด์ได้แต่ไม่ใช่เว็บไซด์ทางการ</p>
+                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                        </div>
+                    </div>
 
 
-<div class="container">
-    <div class="card-group">
-  <div class="card">
-    <img src="https://cdn.pixabay.com/photo/2021/11/12/12/16/leaves-6788800_960_720.jpg" class="card-img-top" >
-    <div class="card-body">
-      <h5 class="card-title">Elastic Beanstlak </h5>
-      <p class="card-text">สร้างenvของเว็บไซด์</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-    </div>
-  </div>
-  <div class="card">
-    <img src="https://cdn.pixabay.com/photo/2021/11/12/12/16/leaves-6788800_960_720.jpg" class="card-img-top" >
-    <div class="card-body">
-      <h5 class="card-title">Amazon RDS</h5>
-      <p class="card-text">database endpoint ของ port 3306 เพือไปเชื่อมกับ mysql workbench</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-    </div>
-  </div>
-  <div class="card">
-    <img src="https://cdn.pixabay.com/photo/2021/11/12/12/16/leaves-6788800_960_720.jpg" class="card-img-top" >
-    <div class="card-body">
-      <h5 class="card-title">MySQL WorkBench</h5>
-      <p class="card-text">สร้าง database</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-</div>
-</div>
-  </div>
-</div>
-</div>
-  </body>
+                    <div class="container">
+                        <div class="card-group">
+                            <div class="card">
+                                <img src="https://cdn.pixabay.com/photo/2021/11/12/12/16/leaves-6788800_960_720.jpg"
+                                    class="card-img-top">
+                                <div class="card-body">
+                                    <h5 class="card-title">Elastic Beanstlak </h5>
+                                    <p class="card-text">สร้างenvของเว็บไซด์</p>
+                                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <img src="https://cdn.pixabay.com/photo/2021/11/12/12/16/leaves-6788800_960_720.jpg"
+                                    class="card-img-top">
+                                <div class="card-body">
+                                    <h5 class="card-title">Amazon RDS</h5>
+                                    <p class="card-text">database endpoint ของ port 3306 เพือไปเชื่อมกับ mysql workbench
+                                    </p>
+                                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <img src="https://cdn.pixabay.com/photo/2021/11/12/12/16/leaves-6788800_960_720.jpg"
+                                    class="card-img-top">
+                                <div class="card-body">
+                                    <h5 class="card-title">MySQL WorkBench</h5>
+                                    <p class="card-text">สร้าง database</p>
+                                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+</body>
+
 </html>
