c6504e020ccd1ccba869845bbde695c25917958d
assd123ss <111363321+assd123ss@users.noreply.github.com>
2022-09-01 20:08:43 +0700

    cc

--- nav.php
+++ nav.php
@@ -12,9 +12,10 @@
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+        
     </script>
 
-
+<link rel="stylesheet" href="./sa.css">
 </head>
 
 <body>
@@ -69,7 +70,6 @@
 
 
 
-    <div class="container">
 
         <audio id="myAudio" controls autoplay>
             <source src="hotka.mp3">
