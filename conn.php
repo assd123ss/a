36a17730010ffc4b1c33be612e2eb3773dc5ea33
assd123ss <111363321+assd123ss@users.noreply.github.com>
2022-09-11 14:35:08 +0700

    Update conn.php

--- conn.php
+++ conn.php
@@ -2,6 +2,7 @@
 $conn = mysqli_connect("dberp.czr48qkizqnc.ap-southeast-1.rds.amazonaws.com","admin","0881085759Hot","dberp");
 //set utf8
 mysqli_query($conn, "SET NAMES 'utf8'");
+echo date('Y-m-d H:i:s');
 //set time zone
 date_default_timezone_set('Asia/Bangkok');
 
