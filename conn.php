45fd0d3f4d8e6389e1bcfddf7ee07ffcc903c4fa
assd123ss <111363321+assd123ss@users.noreply.github.com>
2022-09-11 14:32:30 +0700

    แก้

--- conn.php
+++ conn.php
@@ -1,3 +1,8 @@
 <?php
 $conn = mysqli_connect("dberp.czr48qkizqnc.ap-southeast-1.rds.amazonaws.com","admin","0881085759Hot","dberp");
+//set utf8
+mysqli_query($conn, "SET NAMES 'utf8'");
+//set time zone
+date_default_timezone_set('Asia/Bangkok');
+
 ?>
